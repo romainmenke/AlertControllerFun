75f2b4a36d41f3afe821862be2b6f6382880d55e
Romain Menke <romainmenke@gmail.com>
2015-10-29 02:56:22 +0100

    updated print

--- CustomAlerts/DissectSubviews.swift
+++ CustomAlerts/DissectSubviews.swift
@@ -25,6 +25,20 @@ extension UIViewStack {
     // this prints an address for each subview, add type checks to find what you need
     private func getSubviewStackRecursive(levels: [Int]) {
         
+        func printInfo(type:String,frame:CGRect,printLevels:[Int]) {
+            
+            let x = NSString(format: "%.0f", frame.origin.x)
+            let y = NSString(format: "%.0f", frame.origin.y)
+            let w = NSString(format: "%.0f", frame.size.width)
+            let h = NSString(format: "%.0f", frame.size.height)
+            
+            print("origin:",x,y)
+            print("size:",w,h)
+            print("type:",type)
+            print("address:",printLevels)
+            print("")
+        }
+        
         var currentLevels = levels
         currentLevels.append(0)
         
@@ -34,20 +48,20 @@ extension UIViewStack {
             
             let subview = subviews[i]
             
-            if subview is UILabel {
-                print("Label")
+            switch subview {
+            case is UILabel :
+                printInfo("Label", frame: subview.frame, printLevels: currentLevels)
+            case is UICollectionViewCell :
+                printInfo("cvCell", frame: subview.frame, printLevels: currentLevels)
+            case is UICollectionView :
+                printInfo("CollectionView", frame: subview.frame, printLevels: currentLevels)
+            default :
+                printInfo("UIView", frame: subview.frame, printLevels: currentLevels)
             }
-            
-            if subview is UICollectionViewCell {
-                print("Cell")
-            }
-            
-            print(subview.frame, "depth:", currentLevels)
+
             subview.getSubviewStackRecursive(currentLevels)
         }
     }
-    
-    
 }
 
 
@@ -57,12 +71,9 @@ extension UIView: UIViewStack {
 
 extension UIView {
     
-    
     // use the printed addresses to access the views. This is safe even if the UIView changes in an update.
     func getSubView(withAddress address:[Int]) -> UIView? {
-        
         return getSubviewFromAddressRecursive(self, address: address)
-        
     }
     
     private func getSubviewFromAddressRecursive(superView:UIView, address:[Int]) -> UIView? {
@@ -74,13 +85,9 @@ extension UIView {
             guard currentView.subviews.count > index else {
                 return nil
             }
-            
             currentView = currentView.subviews[index]
             
         }
-        
         return currentView
-        
     }
-    
 }
